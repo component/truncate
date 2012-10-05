95d991d9728564384fd957102c8f151560d03f3b
Damian Suarez <rdsuarez@gmail.com>
2012-10-05 16:21:11 -0300

    test: add test for prefix/sufix support

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var truncate = require('../');
 
-var killbill = "The lead character, called 'The Bride', was a member of the Deadly Viper Assassination Squad, lead by her lover 'Bill'.";
+var killbill = "The lead character, called `The Bride`, was a member of the Deadly Viper Assassination Squad, lead by her lover `Bill`.";
 
 describe('truncate(string, 20)', function(){
   it('should return a string equal to \'The lead character,\'', function(){
@@ -13,6 +13,18 @@ describe('truncate(string, 20)', function(){
 
   it('should return a string less or equal to 20 characters', function(){
     truncate(killbill, 20).length.should.be.below(21);
+  });
+});
+
+describe('truncate(string, 20, \' ...\')', function(){
+  it('should return a string equal to \'The lead character, ...\'', function(){
+    truncate(killbill, 20, ' ...').should.be.equal('The lead character, ...');
+  });
+});
+
+describe('truncate(string, 20, \' …\')', function(){
+  it('should return a string equal to \'The lead character, …\'', function(){
+    truncate(killbill, 20, ' …').should.be.equal('The lead character, …');
   });
 });
 
@@ -31,8 +43,8 @@ describe('truncate.left(string, 20)', function(){
 })
 
 describe('truncate.right(string, 20)', function(){
-  it('should return a string equal to \'her lover \'Bill\'.', function(){
-    truncate.right(killbill, 20).should.be.equal('her lover \'Bill\'.');
+  it('should return a string equal to \'her lover `Bill`.\'', function(){
+    truncate.right(killbill, 20).should.be.equal('her lover `Bill`.');
   });
 
   it('should return a string equal to given string', function(){
@@ -43,3 +55,15 @@ describe('truncate.right(string, 20)', function(){
     truncate.right(killbill, 20).length.should.be.below(21);
   });
 })
+
+describe('truncate.right(string, 20, \'... \')', function(){
+  it('should return a string equal to \'... her lover `Bill`.\'', function(){
+    truncate.right(killbill, 20, '... ').should.be.equal('... her lover `Bill`.');
+  });
+});
+
+describe('truncate.right(string, 20, \'… \')', function(){
+  it('should return a string equal to \'… her lover `Bill`.', function(){
+    truncate.right(killbill, 20, '… ').should.be.equal('… her lover `Bill`.');
+  });
+});
